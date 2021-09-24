5f3dc13ab3cba1b0538ed98cfb12f8c9a582727d
Melissa <melissa.scmi@gmail.com>
2021-09-24 10:13:53 -0700

    pass T4: return populated bread lists

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,9 +1,29 @@
+using System.Collections.Generic;
+using System;
+
 namespace Bakery.Models
 {
   public class Bread
   {
 
-    // properties, methods, etc. will go here.
+    public int Price { get; set; }
+    public static List<Bread> BreadOrder = new List<Bread> {};
+
+    public static List<Bread> GetAll()
+    {
+      return BreadOrder;
+    }
+
+    public static void ClearAll()
+    {
+      BreadOrder.Clear();
+    }
+
+    public Bread(int price)
+    {
+      Price = 5;
+      BreadOrder.Add(this);
+    }
 
   }
 }
