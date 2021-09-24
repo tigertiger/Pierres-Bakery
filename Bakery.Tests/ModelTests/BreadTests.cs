5c39eb5126758afe8e3c8e061ab8b0f1777a6641
Melissa <melissa.scmi@gmail.com>
2021-09-24 10:03:03 -0700

    pass T3: return empty bread list

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -1,16 +1,23 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
 using Bakery.Models;
 
 namespace Bakery.Tests
 {
   [TestClass]
-  public class BreadTests
+  public class BreadTests : IDisposable
   {
+    public void Dispose()
+    {
+      Bread.ClearAll();
+    }
+
     [TestMethod]
-    public void BakeBread_CreateSingleBread_NewBread()
+    public void Bread_CreateSingleBread_NewBread()
     {
       //Arrange
-      string fakeBread = "fakebread";
+      // string fakeBread = "fakebread";
 
       //Act
       Bread testBread = new Bread(5);
@@ -20,17 +27,30 @@ namespace Bakery.Tests
     }
 
     [TestMethod]
-    public void BakeBread_ReturnBreadPrice_Price()
+    public void Bread_ReturnBreadPrice_Price()
     {
       //Arrange
       int price = 5;
+      Bread testBread = new Bread(price);
 
       //Act
-      Bread testBread = new Bread(price);
       int basePrice = testBread.Price;
 
       //Assert
       Assert.AreEqual(price, basePrice);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnEmptyList_BreadList()
+    {
+      //Arrange
+      List<Bread> testOrder = new List<Bread> {};
+      
+      //Act
+      List<Bread> glorbus = Bread.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(testOrder, glorbus);
     }
   }
 }
