dc478d2b5b34b0592f77173b43cefe1454a04dbf
Melissa <melissa.scmi@gmail.com>
2021-09-24 16:16:08 -0700

    pass T8 - T12, recreating methods for pastry

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -1,16 +1,131 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
 using Bakery.Models;
 
 namespace Bakery.Tests
 {
   [TestClass]
-  public class PastryTests
+  public class PastryTests : IDisposable
   {
-    // [TestMethod]
-    // public void NameOfMethod_DescribeBehavior_ExpectedResult()
+    public void Dispose()
+    {
+      Pastry.ClearAll();
+    }
+
+    [TestMethod]
+    public void Pastry_CreateSinglePastry_NewPastry()
+    {
+      //Arrange
+      // string fakePastry = "fakepastry";
+
+      //Act
+      Pastry testPastry = new Pastry(1, 5);
+
+      //Assert
+      Assert.AreEqual(typeof(Pastry), testPastry.GetType());
+    }
+
+    [TestMethod]
+    public void Pastry_ReturnPastryPrice_Price()
+    {
+      //Arrange
+      int price = 5;
+      Pastry testPastry = new Pastry(1, price);
+
+      //Act
+      int basePrice = testPastry.Price;
+
+      //Assert
+      Assert.AreEqual(price, basePrice);
+    }
+
+    [TestMethod]
+    public void GetYourPastries_ReturnEmptyList_PastryList()
+    {
+      //Arrange
+      List<Pastry> testOrder = new List<Pastry> {};
+      
+      //Act
+      List<Pastry> nullOrder = Pastry.GetYourPastries();
+
+      //Assert
+      CollectionAssert.AreEqual(testOrder, nullOrder);
+    }
+
+    [TestMethod]
+    public void GetYourPastries_ReturnPastries_PastryList()
+    {
+      //Arrange
+      int spendy = 33;
+      int basePrice = 5;
+      Pastry cutieCupcake = new Pastry(1, spendy);
+      Pastry baklava = new Pastry(1, basePrice);
+      List<Pastry> PastryOrder = new List<Pastry> {cutieCupcake, baklava};
+
+      //Act
+      List<Pastry> checkOrder = Pastry.GetYourPastries();
+
+      //Assert
+      CollectionAssert.AreEqual(PastryOrder, checkOrder);
+    }
+
+    [TestMethod]
+    public void Pastry_CalculateCrubs_PastryNumber()
+    {
+
+      //Arrange
+      int spendy = 33;
+      int basePrice = 5;
+      Pastry cutieCupcake = new Pastry(1, spendy);
+      Pastry baklava = new Pastry(1, basePrice);
+      List<Pastry> PastryOrder = new List<Pastry> {cutieCupcake, baklava};
+
+      //Act
+      int countCrubs = PastryOrder.Count;
+      int testCount = 2;
+
+      //Assert
+      Assert.AreEqual(testCount, countCrubs);
+    }
+
+    // [TestMethod] 
+    // public void Cashier_CalculateOrderPrice_TotalPrice()
     // {
+    //   // Arrange
+    //   int price = 5;
+    //   Pastry staleCrust = new Pastry(1, price);
+    //   Pastry cutieCupcake = new Pastry(1, price);
+    //   List<Pastry> PastryOrder = new List<Pastry> {staleCrust, cutieCupcake};
+    //   int orderPrice = 10;
+
+    //   //Act
+    //   int testOrderPrice = staleCrust.Cashier();
+
+    //   //Assert
+    //   Assert.AreEqual(orderPrice, testOrderPrice);
 
     // }
 
+    // [TestMethod] 
+    // public void Cashier_CalculateBigOrderPrice_TotalPrice()
+    // {
+    //   // Arrange
+    //   int price = 5;
+    //   Pastry staleCrust = new Pastry(1, price);
+    //   Pastry cutieCupcake = new Pastry(1, price);
+    //   Pastry croissant = new Pastry(1, price);
+    //   Pastry notAPastryAtAll = new Pastry(1, price);
+    //   Pastry birdFeatherPie = new Pastry(1, price);
+    //   List<Pastry> PastryOrder = new List<Pastry> {staleCrust, cutieCupcake, croissant, notAPastryAtAll, birdFeatherPie};
+    //   int orderPrice = 20;
+
+    //   //Act
+    //   int testOrderPrice = staleCrust.Cashier();
+
+    //   //Assert
+    //   Assert.AreEqual(orderPrice, testOrderPrice);
+
+    // }
   }
 }
--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -1,9 +1,56 @@
-namespace File.Models
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace Bakery.Models
 {
   public class Pastry
   {
 
-    // properties, methods, etc. will go here.
+    public int Price { get; set; }
+    public int Crubs {get ; set; }
+    public int OrderPrice { get; set; }
+    public static List<Pastry> PastryOrder = new List<Pastry> {};
+
+    public Pastry(int crubs, int price)
+    {
+      Price = 5;
+      PastryOrder.Add(this);
+      Crubs = crubs;
+    }
+
+    public static List<Pastry> GetYourPastries()
+    {
+      return PastryOrder;
+    }
+
+    public static void ClearAll()
+    {
+      PastryOrder.Clear();
+    }
+
+    public int Cashier() {
+
+      int CustOrder = Crubs * (PastryOrder.Count);
+
+      if (CustOrder > 2) 
+      {
+      return (CustOrder * 5) - (CustOrder/3*5);
+      }
+      else 
+      {
+      return CustOrder * Price;
+      }
+    }
 
   }
 }
+/*
+1: $2
+2: $4
+3: $5
+4: $7
+5: $9
+6: $10
+Every 3rd pastry is -$1
+*/
