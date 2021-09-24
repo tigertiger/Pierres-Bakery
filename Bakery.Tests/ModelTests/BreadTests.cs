f09703565a7996ebcb335f095f20eedeab1e8eb9
Melissa <melissa.scmi@gmail.com>
2021-09-24 11:45:01 -0700

    pass T5: calculate # of loaves

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -41,20 +41,20 @@ namespace Bakery.Tests
     }
 
     [TestMethod]
-    public void GetAll_ReturnEmptyList_BreadList()
+    public void GetYourBreads_ReturnEmptyList_BreadList()
     {
       //Arrange
       List<Bread> testOrder = new List<Bread> {};
       
       //Act
-      List<Bread> glorbus = Bread.GetAll();
+      List<Bread> glorbus = Bread.GetYourBreads();
 
       //Assert
       CollectionAssert.AreEqual(testOrder, glorbus);
     }
 
     [TestMethod]
-    public void GetAll_ReturnBreads_BreadList()
+    public void GetYourBreads_ReturnBreads_BreadList()
     {
       //Arrange
       int spendy = 33;
@@ -65,12 +65,31 @@ namespace Bakery.Tests
       List<Bread> halfOrder = new List<Bread> {sourdoughLoaf};
 
       //Act
-      List<Bread> checkOrder = Bread.GetAll();
+      List<Bread> checkOrder = Bread.GetYourBreads();
 
       //Assert
       CollectionAssert.AreEqual(breadOrder, checkOrder);
+    }
 
+    [TestMethod]
+    public void Bread_CalculateLoaves_BreadNumber()
+    {
+      int spendy = 33;
+      int basePrice = 5;
+      Bread thirtyThreeLbBread = new Bread(spendy);
+      Bread sourdoughLoaf = new Bread(basePrice);
+      List<Bread> breadOrder = new List<Bread> {thirtyThreeLbBread, sourdoughLoaf};
 
+      //Arrange
+      int price = 5;
+      int loaves = 2;
+      Bread testBread = new Bread(price);
+
+      //Act
+      int baseLoaves = testBread.Loaves;
+
+      //Assert
+      Assert.AreEqual(loaves, baseLoaves);
     }
   }
 }
